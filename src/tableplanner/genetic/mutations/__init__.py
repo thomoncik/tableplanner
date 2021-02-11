c75c257d5b7e6a864434d5a49ada50ec2e994a8a
Tomasz Homoncik <tjhomoncik@gmail.com>
2021-02-11 02:13:04 +0100

    Fix swap mutation

--- src/tableplanner/genetic/mutations/__init__.py
+++ src/tableplanner/genetic/mutations/__init__.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def swap_mutation(chromosome):
     idx = range(len(chromosome))
-    i1, i2 = np.random.choice(idx, 2)
+    i1, i2 = np.random.choice(idx, 2, replace=False)
 
     result = chromosome.copy()
     result[i1], result[i2] = result[i2], result[i1]
